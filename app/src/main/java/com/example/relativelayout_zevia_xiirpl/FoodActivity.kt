1bed32aae964038283c0e0f9f12066b47e9b8504
zeviaaaaaaa <zeviazahira12@gmail.com>
2023-02-13 10:53:29 +0700

    Menambahkan Activity Food

---
+++ app/src/main/java/com/example/relativelayout_zevia_xiirpl/FoodActivity.kt
@@ -0,0 +1,15 @@
+package com.example.relativelayout_zevia_xiirpl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food)
+
+
+    }
+
+
+}
